cc211e49b38747a9423fbc32b4825587e25768a5
lamia <lamiazidane213@gmail.com>
2026-04-02 18:43:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.add("show-mobile-menu");
+});
+
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
